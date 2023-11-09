2892a5a78e678e09feecbdb70ab56b385e87f676
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-11-09 14:06:08 +0100

    Initialized project

---
+++ main.c
@@ -0,0 +1,13 @@
+// TODO: Define syntax (BNF/EBNF)
+// TODO: Create lexer
+// TODO: Create parser
+// TODO: Create iterpreter
+// TODO: Test and upgrade
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+}
