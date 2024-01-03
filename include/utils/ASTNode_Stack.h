443c341622b94645203b12a8122bf89bbae6a724
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2024-01-03 22:46:47 +0100

    Created example comments and configured doxygen to generate whole project documentation @WiktorB2004 #5

--- include/utils/ASTNode_Stack.h
+++ include/utils/ASTNode_Stack.h
@@ -9,8 +9,17 @@ typedef struct
     int top;
 } ASTNodeStack;
 
+/*!
+ * Function responsible for the correct initialization of ASTNode Stack
+ */
 ASTNodeStack *create_ASTNodeStack();
+
+/*!
+ * Function checking if the ASTNode stack is empty
+ * @param stack Stack to perform check on
+ */
 int is_ASTNodeStack_empty(ASTNodeStack *stack);
+
 int is_ASTNodeStack_full(ASTNodeStack *stack);
 void ASTNodeStack_push(ASTNodeStack *stack, ASTNode *value);
 ASTNode *ASTNodeStack_pop(ASTNodeStack *stack);
