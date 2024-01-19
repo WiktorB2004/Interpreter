1d695178583b314e7d738e1b3824bb6f4cdb1ac3
WiktorB2004 <WiktorB2004@users.noreply.github.com>
2024-01-19 17:48:10 +0000

    deploy: 8de85caec52adf8ac69f5e7f67338d84edf015e9

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var index =
 [
     [ "How to use", "index.html#autotoc_md1", null ],
-    [ "Project Plan / Phases", "index.html#autotoc_md4", [
-      [ "Summary", "index.html#autotoc_md5", null ]
-    ] ],
-    [ "Why C?", "index.html#autotoc_md6", null ]
+    [ "Project Plan / Phases", "index.html#autotoc_md4", null ],
+    [ "Portfolio Project Summary", "index.html#autotoc_md5", [
+      [ "Why C?", "index.html#autotoc_md6", null ]
+    ] ]
 ];
